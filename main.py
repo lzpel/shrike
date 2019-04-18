00ff38317df797dc108d872a10ddca95907fe2bc
misumi3104 <18492524+misumi3104@users.noreply.github.com>
2019-04-18 22:12:02 +0900

    a

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 from template.appengine import *
 from template.unit import *
 from datetime import datetime,timedelta
+INTERVAL_MIN=10
 CLIENT_ID="342204927924.592004947603"
 CLIENT_SECRET="fd05589134031d6b61048a445041ac13"
-OAUTH_TOKEN="xoxb-342204927924-595037105715-V0Cn4SmjDyQiVYrYuy5dUii2"
+OAUTH_BOT_TOKEN= "xoxb-342204927924-595037105715-V0Cn4SmjDyQiVYrYuy5dUii2"
 def testfunc(request, *args, **kwargs):
 	r=http.get("http://info.cern.ch/hypertext/WWW/TheProject.html")
 	return textres('You requested product<br>{0}<br>{1}'.format(json.dumps(args),json.dumps(kwargs)))
@@ -20,7 +21,7 @@ def helloslack(request,*args,**kwargs):
 			if d=="message":
 				unit(area="message",smalljson=c).put()
 				http.post("https://slack.com/api/chat.postMessage", {
-					'token': OAUTH_TOKEN,
+					'token': OAUTH_BOT_TOKEN,
 					'channel': c['channel'],
 					'text': c["text"],
 					'username': 'shrike',
@@ -38,30 +39,57 @@ def datetimecast(m):
 	format="%Y-%m-%d"
 	tz=timedelta(hours=+9)
 	return datetime.strptime((m+tz).strftime(format),format)-tz
+def postpresence(request,presence):
+	text=str()
+	for i in presence.smalljson:
+		text+=i["name"]
+		for i,n in enumerate(i["presence"]):
+			if i%(60/INTERVAL_MIN)==0:
+				text+=" "
+			if i%(6*60/INTERVAL_MIN)==0:
+				text+="\n"
+			text+="01"[int(n)]
+		text+="\n"
+	status,bodylist=http.get("https://slack.com/api/channels.list", {'token': OAUTH_BOT_TOKEN}, datatype="json")
+	if bodylist["ok"]:
+		for i in bodylist["channels"]:
+			if i["is_member"]:
+				http.post("https://slack.com/api/chat.postMessage", {
+					'token': OAUTH_BOT_TOKEN,
+					'channel': i['id'],
+					'text': text,
+					'username': 'shrike',
+					'icon_url': urlformat("{host}/icon.png",request,None),
+				})
 
 def presence(request):
-	presence=unit.query(unit.area=="presence",unit.born>(datetimecast(datetime.now())-timedelta(days=1))).fetch()
+	presence=unit.query(unit.area=="presence").order(-unit.born).fetch()
+	#最新以外不要
 	unit.delete_multi(i.key for i in presence[1:])
+	presence=presence and presence[0]
+	#当日チェックと投稿
+	if presence and datetimecast(presence.born) < datetimecast(datetime.now()):
+		postpresence(request, presence)
+		presence=None
+	#存在しないなら作成
 	if not presence:
-		presence=unit(area="presence")
+		status,bodylist=http.get("https://slack.com/api/users.list", {'token': OAUTH_BOT_TOKEN}, datatype="json")
+		if bodylist["ok"]:
+			member=[i for i in bodylist["members"] if not (i["is_bot"] or i["id"]=="USLACKBOT")]
+			for i in member:
+				i["presence"]=[]
+		presence=unit(area="presence",smalljson=member)
 		presence.put()
-
-	result=[]
-	status,bodylist=http.get("https://slack.com/api/users.list", {'token': OAUTH_TOKEN},datatype="json")
-	if bodylist["ok"]:
-		for member in bodylist["members"]:
-			if not ( member["is_bot"] or member["id"]=="USLACKBOT" ):
-				status, bodypres = http.get("https://slack.com/api/users.getPresence", {
-					'token': OAUTH_TOKEN,
-					"user":member["id"]
-				}, datatype="json")
-				result.append({
-					"id":member["id"],
-					"name":member["name"],
-					"presence":(bodypres["presence"]=="active")
-				})
-		unit(area="presense",smalljson=result).put()
-	return jsonres(result)
+	#勤怠追加
+	for i in presence.smalljson:
+		status, bodypres = http.get("https://slack.com/api/users.getPresence", {
+			'token': OAUTH_BOT_TOKEN,
+			"user": i["id"]
+		}, datatype="json")
+		i["presence"].append(bodypres["presence"] == "active")
+	#保存
+	presence.put()
+	return jsonres(presence.smalljson)
 def oauthsend(request):
 	return passres(urlformat("https://slack.com/oauth/authorize?{params}",None,{
 		"client_id":CLIENT_ID,
